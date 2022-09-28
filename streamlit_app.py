02b04c03cf6b908a8403c404a4611b1f1f0261f9
cpharika <114474124+cpharika@users.noreply.github.com>
2022-09-28 14:33:10 +0530

    Update streamlit_app.py
    Breakfast meanu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
+streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.header('hi harika')
-
-streamlit.title('hoe r u')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
